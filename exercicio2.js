25a2a7442587a7dbde3a5de30e55a2cebe78d398
agatharbrito <agatharodrigues234@gmail.com>
2024-01-19 22:37:05 -0300

    Adicionando o exercicio 2 de node.js

---
+++ exercicio2.js
@@ -0,0 +1,11 @@
+// Criação de Variáveis
+const cidade = "Saquarema";
+let populacao = 45000;
+let taxaCrescimento = 0.13;
+
+populacao = populacao + 1000;
+taxaCrescimento = taxaCrescimento * 1.5;
+
+console.log(`Nome da Cidade: ${cidade}`);
+console.log(`Nova População: ${populacao}`);
+console.log(`Taxa de Crescimento: ${taxaCrescimento}`);
