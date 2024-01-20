cf309f97038d27d2c7586fbadbf38b90ffb08837
agatharbrito <agatharodrigues234@gmail.com>
2024-01-19 22:58:05 -0300

    Adicionando o exercício 2 de node.js

--- exercicio2.js
+++ exercicio2.js
@@ -3,7 +3,7 @@ const cidade = "Saquarema";
 let populacao = 45000;
 let taxaCrescimento = 0.13;
 
-populacao = populacao + 1000;
+populacao = populacao + 10000;
 taxaCrescimento = taxaCrescimento * 1.5;
 
 console.log(`Nome da Cidade: ${cidade}`);
