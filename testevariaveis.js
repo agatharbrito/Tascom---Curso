7e34d8c5495a6b013bf7399eb541eff7666037fd
agatharbrito <agatharodrigues234@gmail.com>
2024-01-19 22:13:18 -0300

    Adicionando testes de variáveis

---
+++ testevariaveis.js
@@ -0,0 +1,20 @@
+//escovar os dentes
+
+var pasta = "pasta";
+var escova = "escova";
+var minutos = 5;
+
+//escovar os dentes
+
+var escovarOsDentes =
+  "pegar a " +
+  pasta +
+  " e a " +
+  escova +
+  " e escovar os dentes por " +
+  minutos +
+  "minutos";
+
+var escovar_os_dentes2 = `pegar a ${pasta} e a ${escova} e escovar os dentes por ${minutos} minutos`;
+
+console.log(escovarOsDentes);
---
+++ testevariaveis2.js
@@ -0,0 +1,17 @@
+var num1 = 4;
+var num2 = 2;
+var resultado = num1 * num2;
+
+console.log(`o resultado de ${num1} * ${num2} e ${resultado}`);
+
+var num1 = 30;
+var num2 = 4;
+var resultado = num1 / num2;
+
+console.log(`o resultado de ${num1} / ${num2} e ${resultado}`);
+
+var num1 = 50;
+var num2 = 17;
+var resultado = num1 - num2;
+
+console.log(`o resultado de ${num1} - ${num2} e ${resultado}`);
