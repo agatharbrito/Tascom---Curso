3517eba361709f4e3100ae173b63acd428a38f1b
agatharbrito <agatharodrigues234@gmail.com>
2024-01-19 22:55:07 -0300

    Adicionando exercício 1 de node.js

--- exercicio1.js
+++ exercicio1.js
@@ -6,6 +6,6 @@ let precoUnitario = 14.99;
 quantidade = quantidade + 5;
 precoUnitario = precoUnitario * 1.1;
 
-console.log(`Produto: ${produto}`);
-console.log(`Quantidade: ${quantidade}`);
-console.log(`PrecoUnitario: ${precoUnitario}`);
+console.log(`Nome do produto: ${produto}`);
+console.log(`A nova quantidade do produto e: ${quantidade}`);
+console.log(`O novo preco unitario e: ${precoUnitario.toFixed(2)}`);
