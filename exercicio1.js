1db50e927479944b2c3b05117d12654732ebd746
agatharbrito <agatharodrigues234@gmail.com>
2024-01-19 22:25:44 -0300

    Adicionando o exercicio 1 de node.js

---
+++ exercicio1.js
@@ -0,0 +1,11 @@
+// Criacao de variaveis
+const produto = "batom";
+let quantidade = 3;
+let precoUnitario = 14.99;
+
+quantidade = quantidade + 5;
+precoUnitario = precoUnitario * 1.1;
+
+console.log(`Produto: ${produto}`);
+console.log(`Quantidade: ${quantidade}`);
+console.log(`PrecoUnitario: ${precoUnitario}`);
