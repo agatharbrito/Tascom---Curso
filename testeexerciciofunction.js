2549b4742cc7828b0d11f1eee45ed07c815c5c43
agatharbrito <agatharodrigues234@gmail.com>
2024-04-13 17:15:33 -0300

    Função para classificar o dia da semana

---
+++ testeexerciciofunction.js
@@ -0,0 +1,20 @@
+function diaDaSemana(dia) {
+  switch (dia) {
+    case 1:
+      return "domingo";
+    case 2:
+      return "segunda - feira";
+    case 3:
+      return "terça - feira";
+    case 4:
+      return "quarta - feira";
+    case 5:
+      return "quinta - feira";
+    case 6:
+      return "sexta - feira";
+    case 7:
+      return "sabado";
+  }
+}
+
+console.log(diaDaSemana(7));
